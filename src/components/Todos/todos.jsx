7b5185244bbd1301fa3503a70a320ea8e5180ff3
avinek <avinek.p.kavil@gmail.com>
2024-05-10 15:36:02 +0530

    Requirements functional.

--- src/components/Todos/todos.jsx
+++ src/components/Todos/todos.jsx
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import {
   Checkbox,
   FormControlLabel,
   FormGroup,
   IconButton,
+  Dialog,
+  DialogTitle,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemAvatar,
+  Avatar,
+  ListItemText,
+  Button,
+  TextField,
 } from "@mui/material";
 import ClearIcon from "@mui/icons-material/Clear";
 import EditIcon from "@mui/icons-material/Edit";
@@ -11,15 +21,16 @@ import axios from "axios";
 import "./todo.css";
 
 const Todo = ({ description, todoId, projectId, checkedValue }) => {
-  console.log("description", description);
+  const [openModal, setOpenModal] = useState(false);
+  const [editedDescription, setEditedDescription] = useState(description);
 
   const handleCheckboxChange = async () => {
     try {
       const token = localStorage.getItem("token");
       const response = await axios.put(
-        `http://localhost:5000/todo/${todoId}`, // Use the correct endpoint
+        `http://localhost:5000/todo/${todoId}`,
         {
-          description: description,
+          description: editedDescription,
         },
         {
           headers: {
@@ -40,7 +51,7 @@ const Todo = ({ description, todoId, projectId, checkedValue }) => {
     try {
       const token = localStorage.getItem("token");
       const response = await axios.delete(
-        `http://localhost:5000/todos/${todoId}/${projectId}`, // Use the correct endpoint
+        `http://localhost:5000/todos/${todoId}/${projectId}`,
         {
           headers: {
             Authorization: `Bearer ${token}`,
@@ -52,6 +63,38 @@ const Todo = ({ description, todoId, projectId, checkedValue }) => {
       window.location.reload();
     } catch (error) {
       console.error("Error deleting todo:", error);
+    }
+  };
+
+  const handleEdit = () => {
+    setOpenModal(true);
+  };
+
+  const handleCloseModal = () => {
+    setOpenModal(false);
+  };
+
+  const handleSaveEdit = async () => {
+    setOpenModal(false);
+    try {
+      const token = localStorage.getItem("token");
+      const response = await axios.put(
+        `http://localhost:5000/todos/${todoId}`,
+        {
+          description: editedDescription,
+        },
+        {
+          headers: {
+            Authorization: `Bearer ${token}`,
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      console.log("Todo updated successfully:", response.data);
+
+      window.location.reload();
+    } catch (error) {
+      console.error("Error updating todo:", error);
     }
   };
 
@@ -70,13 +113,40 @@ const Todo = ({ description, todoId, projectId, checkedValue }) => {
         />
       </FormGroup>
       <div className="todo-actions">
-        <IconButton aria-label="edit">
+        <IconButton aria-label="edit" onClick={handleEdit}>
           <EditIcon />
         </IconButton>
         <IconButton aria-label="delete" onClick={handleDelete}>
           <ClearIcon />
         </IconButton>
       </div>
+      <Dialog open={openModal} onClose={handleCloseModal}>
+        <DialogTitle>Edit Todo</DialogTitle>
+        <List>
+          <ListItem>
+            <ListItemAvatar>
+              <Avatar>
+                <EditIcon />
+              </Avatar>
+            </ListItemAvatar>
+            <ListItemText>
+              <TextField
+                label="Description"
+                variant="outlined"
+                value={editedDescription}
+                onChange={(e) => setEditedDescription(e.target.value)}
+              />
+            </ListItemText>
+          </ListItem>
+          <ListItem>
+            <ListItemButton onClick={handleSaveEdit}>
+              <Button variant="contained" color="primary">
+                Save
+              </Button>
+            </ListItemButton>
+          </ListItem>
+        </List>
+      </Dialog>
     </div>
   );
 };
