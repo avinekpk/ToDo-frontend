e3eaefc9f3a0787aa475ff9075f20913369f7e95
avinek <avinek.p.kavil@gmail.com>
2024-05-10 15:44:51 +0530

    added token check

--- src/App.js
+++ src/App.js
@@ -5,17 +5,20 @@ import Signup from "./pages/Signup/signup";
 import Home from "./pages/Home/home";
 import TodoPage from "./pages/TodoPage/todoPage";
 import NewProject from "./pages/NewProject/NewProject";
+import Layout from "./components/Layout/layout";
 
 function App() {
   return (
     <div>
       <BrowserRouter>
         <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route path="/home" element={<Home />} />
+            <Route path="/project/:id" element={<TodoPage />} />
+            <Route path="/newproject" element={<NewProject />} />
+          </Route>
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<Signup />} />
-          <Route path="/home" element={<Home />} />
-          <Route path="/project/:id" element={<TodoPage />} />
-          <Route path="/newproject" element={<NewProject />} />
         </Routes>
       </BrowserRouter>
     </div>
