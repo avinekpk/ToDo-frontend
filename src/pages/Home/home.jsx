7fb09c08ee3b1fd7a86eb4c1212c78f0ad21b518
avinek <avinek.p.kavil@gmail.com>
2024-05-10 17:39:13 +0530

    home styled

--- src/pages/Home/home.jsx
+++ src/pages/Home/home.jsx
@@ -33,28 +33,32 @@ const Home = () => {
   };
 
   return (
-    <div className="homePage">
-      <div className="header">
-        <button
-          className="new-project-button"
-          onClick={() => navigate("/newproject")}
-        >
-          <AddIcon /> New Project
-        </button>
-        <h1>Projects</h1>
-        <button onClick={handleLogout}>Logout</button>
+    <div className="homePage-box">
+      <div className="homePage">
+        <div className="header">
+          <h1>Projects</h1>
+          <button
+            className="new-project-button"
+            onClick={() => navigate("/newproject")}
+          >
+            <AddIcon /> New Project
+          </button>
+          <button onClick={handleLogout}>Logout</button>
+        </div>
+        <div className="hometiles-container">
+          {projects.map((item) => (
+            <Hometile
+              tilename={item.title}
+              projectId={item._id}
+              key={item._id}
+              onClick={(e) => {
+                e.stopPropagation();
+                navigate(`/project/${item._id}`);
+              }}
+            />
+          ))}
+        </div>
       </div>
-      {projects.map((item) => (
-        <Hometile
-          tilename={item.title}
-          projectId={item._id}
-          key={item._id}
-          onClick={(e) => {
-            e.stopPropagation();
-            navigate(`/project/${item._id}`);
-          }}
-        />
-      ))}
     </div>
   );
 };
